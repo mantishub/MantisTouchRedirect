79e23f43efecf9f6ca8dc4e0f4c9252fc97a77d1
Victor Boctor <victor@mantishub.net>
2014-12-21 14:29:51 -0800

    Modern UI support

--- pages/config.php
+++ pages/config.php
@@ -4,35 +4,56 @@
 
 auth_reauthenticate();
 access_ensure_global_level( config_get( 'manage_plugin_threshold' ) );
-html_page_top1( plugin_lang_get( 'title' ) );
-html_page_top2();
+
+layout_page_header( plugin_lang_get( 'title' ) );
+layout_page_begin( __FILE__ );
+
 print_manage_menu();
 
 $t_mantistouch_url = plugin_config_get( 'mantistouch_url' );
 ?>
-<br/>
-<form action="<?php echo plugin_page( 'config_edit' ) ?>" method="post">
-<table align="center" class="width50" cellspacing="1">
-<tr>
-	<td class="form-title" colspan="3">
-		<?php echo plugin_lang_get( 'title' ) . ': ' . plugin_lang_get( 'config' ) ?>
-	</td>
-</tr>
-<tr <?php echo helper_alternate_class() ?>>
-	<td class="category">
-		<?php echo plugin_lang_get( 'mantistouch_url' ) ?>
-	</td>
-	<td>
-		<input name="mantistouch_url" type="text" size="80" value="<?php echo $t_mantistouch_url; ?>" />
-	</td>
-</tr> 
-<tr>
-	<td class="center" colspan="3">
-		<input type="submit" class="button" value="<?php echo plugin_lang_get( 'update_config' ) ?>" />
-	</td>
-</tr>
-</table>
-</form>
+
+<div class="col-xs-12 col-md-10 col-md-offset-1">
+	<div class="space-10"></div>
+
+<div id="edit-user-div" class="form-container">
+	<form id="edit-user-form" method="post" action="<?php echo plugin_page( 'config_edit' ) ?>">
+		<div class="widget-box widget-color-blue2">
+			<div class="widget-header widget-header-small">
+				<h4 class="widget-title lighter">
+					<i class="ace-icon fa fa-user"></i>
+					<?php echo plugin_lang_get( 'title' ) . ': ' . plugin_lang_get( 'config' ) ?>
+				</h4>
+			</div>
+		<div class="widget-body">
+		<div class="widget-main no-padding">
+		<div class="form-container">
+		<div class="table-responsive">
+		<table class="table table-bordered table-condensed table-striped">
+		<fieldset>
+			<!-- mantistouch_url -->
+			<tr>
+				<td class="category">
+					<?php echo plugin_lang_get( 'mantistouch_url' ) ?>
+				</td>
+				<td>
+					<input id="mantistouch_url" type="text" class="input-sm" size="50" name="mantistouch_url" value="<?php echo string_attribute( $t_mantistouch_url ) ?>" />
+				</td>
+			</tr>
+		</fieldset>
+		</table>
+		</div>
+		</div>
+		</div>
+
+		<div class="widget-toolbox padding-8 clearfix">
+			<input type="submit" class="btn btn-primary btn-white btn-round" value="<?php echo plugin_lang_get( 'update_config' ) ?>" />
+		</div>
+		</div>
+		</div>
+	</form>
+</div>
+<div class="space-10"></div>
 
 <?php
-html_page_bottom1( __FILE__ );
+layout_page_end();
